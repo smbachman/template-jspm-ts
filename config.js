1a6fe5b3ba534dccc3c365e490ebe19d7945067e
Scott Bachman <scott.bachman@gmail.com>
2016-01-20 16:18:58 -0500

    added chai

--- config.js
+++ config.js
@@ -30,6 +30,7 @@ System.config({
   },
 
   map: {
+    "chai": "npm:chai@3.4.1",
     "testcheck": "npm:testcheck@0.1.4",
     "toastr": "github:CodeSeven/toastr@2.1.2",
     "ts": "github:frankwallis/plugin-typescript@2.4.7",
@@ -39,6 +40,51 @@ System.config({
     },
     "github:frankwallis/plugin-typescript@2.4.7": {
       "typescript": "npm:typescript@1.7.5"
+    },
+    "github:jspm/nodelibs-assert@0.1.0": {
+      "assert": "npm:assert@1.3.0"
+    },
+    "github:jspm/nodelibs-buffer@0.1.0": {
+      "buffer": "npm:buffer@3.6.0"
+    },
+    "github:jspm/nodelibs-process@0.1.2": {
+      "process": "npm:process@0.11.2"
+    },
+    "github:jspm/nodelibs-util@0.1.0": {
+      "util": "npm:util@0.10.3"
+    },
+    "npm:assert@1.3.0": {
+      "util": "npm:util@0.10.3"
+    },
+    "npm:buffer@3.6.0": {
+      "base64-js": "npm:base64-js@0.0.8",
+      "child_process": "github:jspm/nodelibs-child_process@0.1.0",
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "ieee754": "npm:ieee754@1.1.6",
+      "isarray": "npm:isarray@1.0.0",
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:chai@3.4.1": {
+      "assertion-error": "npm:assertion-error@1.0.1",
+      "buffer": "github:jspm/nodelibs-buffer@0.1.0",
+      "deep-eql": "npm:deep-eql@0.1.3",
+      "process": "github:jspm/nodelibs-process@0.1.2",
+      "systemjs-json": "github:systemjs/plugin-json@0.1.0",
+      "type-detect": "npm:type-detect@1.0.0"
+    },
+    "npm:deep-eql@0.1.3": {
+      "buffer": "github:jspm/nodelibs-buffer@0.1.0",
+      "type-detect": "npm:type-detect@0.1.1"
+    },
+    "npm:inherits@2.0.1": {
+      "util": "github:jspm/nodelibs-util@0.1.0"
+    },
+    "npm:process@0.11.2": {
+      "assert": "github:jspm/nodelibs-assert@0.1.0"
+    },
+    "npm:util@0.10.3": {
+      "inherits": "npm:inherits@2.0.1",
+      "process": "github:jspm/nodelibs-process@0.1.2"
     }
   }
 });
--- src/simple/squaring-tests.ts
+++ src/simple/squaring-tests.ts
@@ -1,7 +1,10 @@
 import assertCheck from "../testing/assertCheck"
 import {property, gen} from "testcheck";
+import {expect} from "chai";
 import {square} from "./squaring";
 
 assertCheck(property([gen.int], function (x) {
     return square(x) === x * x;
 }));
+
+expect(square(4)).to.equal(16);
